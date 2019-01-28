182d42d95a0dd83cd2d8192e0dd86353ee30c99d
Ulya Harlamova <tellera111@gmail.com>
2019-01-28 12:40:16 +0300

    Added snakes move, creating apple, checking end of game

--- src/GameField.java
+++ src/GameField.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class GameField extends JPanel {
+public class GameField extends JPanel implements ActionListener {
 
-    private final int SIZE = 320;
-    private final int DOT_SIZE = 16;
+    private final int SIZE = 640;
+    private final int DOT_SIZE = 32;
     private final int ALL_DOTS = 400;
+    private final int FREQUENCY = 250;
     private Image dot;
     private Image apple;
     private int appleX;
@@ -14,17 +18,90 @@ public class GameField extends JPanel {
     private int[] y = new int[ALL_DOTS];
     private int sizeSnake;
     private Timer timer;
-    private boolean left, right, up = true, down, inGame = true;
+    private boolean left, right, up, down = true, inGame = true;
 
     public GameField() {
         setBackground(Color.black);
         loadImages();
+        initGame();
+    }
+
+    public void initGame() {
+        sizeSnake = 3;
+        for (int i = 0; i < sizeSnake; i++) {
+            x[i] = 48;
+            y[i] = 48 + i*DOT_SIZE;
+        }
+        timer = new Timer(FREQUENCY, this);
+        timer.start();
+        createApple();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < sizeSnake; i++) {
+                g.drawImage(dot, x[i], y[i], this);
+            }
+        }
+    }
+
+    public void createApple() {
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
     }
 
     private void loadImages() {
-        ImageIcon appleIcon = new ImageIcon("apple.svg");
+        ImageIcon appleIcon = new ImageIcon("apple3.png");
         apple = appleIcon.getImage();
-        ImageIcon dotIcon = new ImageIcon("blank-check-box.svg");
+        ImageIcon dotIcon = new ImageIcon("square.png");
         dot = dotIcon.getImage();
+    }
+
+    public void move() {
+        for (int i = sizeSnake; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left) {
+            x[0] -= DOT_SIZE;
+        }
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+        if (up) {
+            y[0] -= DOT_SIZE;
+        }
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    public void checkApple() {
+        if (x[0] == appleX && y[0]==appleY) {
+            sizeSnake++;
+            createApple();
+        }
+    }
+
+    public void checkBorders() {
+        for (int i = sizeSnake; i > 0; i--) {
+            if (i>4 && x[0] == x[i] && y[0] == y[i])
+                inGame = false;
+        }
+        if (x[0] < 0 || x[0] > SIZE || y[0] < 0 || y[0] > SIZE)
+            inGame = false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkBorders();
+            move();
+        }
+        repaint();
     }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -5,13 +5,13 @@ public class MainWindow extends JFrame {
     public MainWindow() {
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(320, 345);
-        setLocation(400, 400);
+        setSize(640, 640);
+        setLocation(200, 100);
         add(new GameField());
         setVisible(true);
     }
 
-    public static void main() {
+    public static void main(String[] args) {
         MainWindow mw = new MainWindow();
     }
 }
